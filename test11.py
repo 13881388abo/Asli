8dadee0a0add8b3a0cf1bce310142609cd9e2f11
Abolfzl <139365194+13881388abo@users.noreply.github.com>
2023-08-11 17:07:22 +0330

    Add files via upload

---
+++ test11.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    for a in range(10):
+        
+        def bored(a , y):
+            print(a * y)
+        bored((i+1) , (a+1))
---
+++ test12.py
@@ -0,0 +1,25 @@
+import random
+print("rock", "scissor" ,"paper")
+
+lst = ["rock" , "scissor" , "paper"]
+
+a=input()
+b = random.choice(lst)
+
+print("player 1 got==" , a)
+print("player 2 got==" , b)
+
+if a == "rock" and b == "scissor":
+    print("player 1 won")
+elif a == "rock" and b == "paper":
+    print("player 2 won")
+elif a == "scissor" and b == "paper":
+    print("player 1 won")
+elif a == "scissor" and b == "rock":
+    print("player 2 won")
+elif a == "paper" and b == "rock":
+    print("player 1 won")
+elif a == "paper" and b == "scissor":
+    print("player 2 won")
+elif a==b:
+    print("equaled")
